d3283f5c01df464047d8dcab6a6ee9a797b18a53
lizhipeng <845007771@qq.com>
2020-04-16 12:45:54 +0800

    项目目录结构分支

---
+++ src/pages/home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-link ></router-link>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>

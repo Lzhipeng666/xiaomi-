5892aa7c15e73674143ab617ee7498e8f3a7c42d
lizhipeng <845007771@qq.com>
2020-04-20 17:45:05 +0800

    首页轮播功能

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -227,7 +227,7 @@ export default {
   name: "nav-header",
   data() {
     return {
-      // username: "jack",
+      username: "",
       phoneList: []
     };
   },
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,94 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOptions">
+          <swiper-slide v-for="(item,index) in slideList" :key="index">
+            <a :href="'/#/product/'+item.id">
+              <img :src="item.img" />
+            </a>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
     <ServiceBar></ServiceBar>
   </div>
 </template>
 <script>
 import ServiceBar from "../components/ServiceBar";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 export default {
   name: "index",
   data() {
-    return {};
+    return {
+      swiperOptions: {
+        autoplay: true /**自动播放 */,
+        loop: true /**循环 */,
+        effect: "cube" /**指定动画 */,
+        cubeEffect: {
+          /**阴影 */
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        pagination: {
+          /**分页 */
+          el: ".swiper-pagination",
+          clickable: true /**点击分页 */
+        },
+        navigation: {
+          /**左右箭头点击事件 */
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg"
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg"
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg"
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-4.jpg"
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-5.jpg"
+        }
+      ]
+    };
   },
   components: {
+    swiper,
+    swiperSlide,
     ServiceBar
   }
 };
 </script>
+<style lang="scss">
+.index {
+  .swiper-box {
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>

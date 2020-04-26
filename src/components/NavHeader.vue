549b16a48251cc000777331790714463789eccb0
lizhipeng <845007771@qq.com>
2020-04-26 14:38:46 +0800

    order-header组件提取为公用组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -296,30 +296,7 @@ export default {
       position: relative;
       height: 112px;
       @include flex();
-      .header-logo {
-        display: inline-block;
-        width: 55px;
-        height: 55px;
-        background-color: #ff6600;
-        a {
-          display: inline-block;
-          width: 110px;
-          height: 55px;
-          &:before {
-            content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
-            transition: margin 0.2s;
-          }
-          &:after {
-            content: "";
-            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
-          }
-          &:hover:before {
-            margin-left: -55px;
-            transition: margin 0.2s;
-          }
-        }
-      }
+
       .header-menu {
         display: inline-block;
         width: 643px;
